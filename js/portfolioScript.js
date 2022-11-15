3becf112148ab552b8e59565d963b3d30b83c3f9
Iyanna Alani <87724081+IyannaB623@users.noreply.github.com>
2022-11-15 11:38:20 -0800

    Update portfolioScript.js

--- js/portfolioScript.js
+++ js/portfolioScript.js
@@ -9,16 +9,23 @@ const projects = [
     },
     {
         photo: "IMG/Screen Shot 2022-03-24 at 12.20.06 PM.png",
-        alt: "This is what its about",
+        alt: "",
         title: "Her Boxing Gym",
         description: 'A boxing gym exclusively open for "girl power" only, Her Boxing Gym. This layout captures the grit, strength, and boldness the Her Boxing Gym carries.',
         link: "underConstruction.html"
     },
      {
         photo: "IMG/studioghibligif.gif",
-        alt: "This is what its about",
+        alt: "A gif of studio ghibli api project website",
         title: "Studio Ghibli API Project",
         description: 'A Studio Ghibli film gallery inspired site using the ghibliapi.herokuapp.com',
         link: "https://studioghibliapiproject.netlify.app/"
+    },
+    {
+        photo: "IMG/tempconvertergif.gif",
+        alt: "A gif of temperature converter website",
+        title: "Temperature Converter",
+        description: 'This project is self expanatory in its title. It was built using HTML, CSS, and JavaScript. The converter will convert any numeric value interchangeably between Celsius, Fahrenheit, and Kelvin.',
+        link: "https://temperatureconverterbyid.netlify.app/"
     },
 ];
