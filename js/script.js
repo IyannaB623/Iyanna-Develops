412f8e1e56e9b4a8f67bb330a6cbf0c430df2a83
Iyanna Alani <87724081+IyannaB623@users.noreply.github.com>
2022-11-18 19:50:13 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,10 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+var req = new XMLHttpRequest();
+req.open('GET', 'http://labs.bible.org/api/?passage=votd&formatting=plain ', false);
+req.send(null);
+console.log(req.responseText);
+
+let votd = document.getElementById('votd').innerText = req.responseText 
