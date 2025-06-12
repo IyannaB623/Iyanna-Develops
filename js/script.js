dcec5e79744515be4404d696bc3252a8320e10dc
Iyanna Alani <87724081+IyannaB623@users.noreply.github.com>
2025-06-12 01:01:13 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,44 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const track = document.querySelector('.carousel-track');
-  const slides = Array.from(track.children);
-  const nextBtn = document.querySelector('.carousel-btn.right');
-  const prevBtn = document.querySelector('.carousel-btn.left');
-
-  let currentSlide = 0;
-
-  function updateSlide() {
-    const slideWidth = slides[0].getBoundingClientRect().width;
-    track.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
-
-    // Hide left arrow on first slide
-    if (currentSlide === 0) {
-      prevBtn.style.display = 'none';
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += "responsive";
     } else {
-      prevBtn.style.display = 'block';
-    }
-
-    // Hide right arrow on last slide
-    if (currentSlide === slides.length - 1) {
-      nextBtn.style.display = 'none';
-    } else {
-      nextBtn.style.display = 'block';
+      x.className = "topnav";
     }
   }
 
-  nextBtn.addEventListener('click', () => {
-    if (currentSlide < slides.length - 1) {
-      currentSlide++;
-      updateSlide();
-    }
-  });
+const options = {method: 'GET', headers: {accept: 'application/json'}};
 
-  prevBtn.addEventListener('click', () => {
-    if (currentSlide > 0) {
-      currentSlide--;
-      updateSlide();
-    }
-  });
+let votd = document.getElementById('votd')
+let ref = document.getElementById('reference')
 
-  window.addEventListener('resize', updateSlide);
-  updateSlide(); // initial update
-});
+fetch('https://beta.ourmanna.com/api/v1/get?format=json&order=daily', options)
+  .then(data => data.json())
+  .then(data => {
+  console.log(data.verse.details);
+  votd.innerText = data.verse.details.text;
+  ref.innerText = data.verse.details.reference
+}) 
+  .catch(err => console.error(err));
